8a6ed9ce33bcc2c425a68ee211030ea19083d09d
NotArceuid <nevo080902@gmail.com>
2026-01-19 19:10:41 +0800

    Changed build

--- svelte.config.js
+++ svelte.config.js
@@ -3,18 +3,22 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      fallback: 'index.html'
+      fallback: 'index.html',
+      pages: 'build',
+      assets: 'build',
+      precompress: false,
+      strict: false
     }),
     paths: {
-      base: process.env.NODE_ENV == 'production' ? '/soap-game' : ''
-    }
-  },
-  compilerOptions: {
-    customElement: true
-  },
-  preprocess: vitePreprocess(),
+      base: process.env.NODE_ENV === 'production' ? '/soap-game' : '',
+      relative: false
+    },
+    appDir: '_app'
+  }
 };
+
 
 export default config;
